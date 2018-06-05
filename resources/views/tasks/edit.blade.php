5d5f65cd23fd6eaf4146a2e0dee48faf3dbb79ef
takuya uchida <takuya.uchida@rakuten.com>
2018-06-05 07:15:18 +0000

    revised

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,7 +4,7 @@
 
 <!-- Write content for each page here -->
 	<h1>id = {{ $task->id }}のタスク編集ページ</h1>
-	<div class="col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+	<div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 	{!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 	<div class="form-group">
 		{!! Form::label('content', 'タスク') !!}
